137c4893a8a93f310e8f78ccff0aa36f6e72a7be
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-13 08:27:22 +0500

    Task3 and Task 5 of Hometasks

--- Lab1/HomeTasks/Task3.java
+++ Lab1/HomeTasks/Task3.java
@@ -1,5 +1,18 @@
 package Lab1.HomeTasks;
 
 public class Task3 {
-  
+  public static void main(String[] args) {
+    Integer num1 = -25;
+    Double num2 = 16.0;
+    Integer power = 3;
+
+    int absValue = Math.abs(num1);
+    System.out.println("Absolute value of " + num1 + " is: " + absValue);
+
+    double sqrtValue = Math.sqrt(num2);
+    System.out.println("Square root of " + num2 + " is: " + sqrtValue);
+
+    double powerValue = Math.pow(num2, power);
+    System.out.println(num2 + " raised to the power of " + power + " is: " + powerValue);
+  }
 }
--- Lab1/HomeTasks/Task5.java
+++ Lab1/HomeTasks/Task5.java
@@ -1,5 +1,19 @@
 package Lab1.HomeTasks;
 
 public class Task5 {
-  
+  public static void main(String[] args) {
+    String sent = "Hi my name is Shahzaib Durrani!";
+    String[] words = sent.split(" ");
+    String longestWord = "";
+    int maxLength = 0;
+
+    for (String word : words) {
+      if (word.length() > maxLength) {
+        longestWord = word;
+        maxLength = word.length();
+      }
+    }
+    System.out.println("The longest word is: " + longestWord);
+    System.out.println("Length of the longest word: " + maxLength);
+  }
 }
