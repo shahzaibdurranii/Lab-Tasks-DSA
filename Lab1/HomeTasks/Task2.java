4c92067542527e96803f0fcb2cccfd8d9a56922a
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-13 08:17:18 +0500

    Task2 of HomeTasks

--- Lab1/HomeTasks/Task2.java
+++ Lab1/HomeTasks/Task2.java
@@ -1,5 +1,24 @@
 package Lab1.HomeTasks;
 
 public class Task2 {
-  
+  public static void main(String[] args) {
+    int num = 123456789;
+    Integer numObject = num;
+    int evenCount = 0;
+    int oddCount = 0;
+    String numString = numObject.toString();
+
+    for (int i = 0; i < numString.length(); i++) {
+      int digit = Character.getNumericValue(numString.charAt(i));
+      if (digit % 2 == 0) {
+        evenCount++;
+      } else {
+        oddCount++;
+      }
+    }
+
+    System.out.println("Number: " + numObject);
+    System.out.println("Count of even digits: " + evenCount);
+    System.out.println("Count of odd digits: " + oddCount);
+  }
 }
