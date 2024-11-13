338697ad140ceaaa9e0455a09e2d9e1b4de38d8c
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-13 08:31:14 +0500

    Task 4

--- Lab1/HomeTasks/Task4.java
+++ Lab1/HomeTasks/Task4.java
@@ -1,5 +1,39 @@
 package Lab1.HomeTasks;
 
 public class Task4 {
+  public static void main(String[] args) {
+          String input = "hello world";
+
+          String result = revVowels(input);
+          System.out.println("Original string: " + input);
+          System.out.println("String with reversed vowels: " + result);
+      }
+      public static String revVowels(String s) {
+          char[] chars = s.toCharArray();
+          int left = 0;
+          int right = chars.length - 1;
+          String vowels = "aeiouAEIOU";
   
-}
+          while (left < right) {
+              while (left < right && vowels.indexOf(chars[left]) == -1) {
+                  left++;
+              }
+
+              while (left < right && vowels.indexOf(chars[right]) == -1) {
+                  right--;
+              }
+
+              if (left < right) {
+                  char temp = chars[left];
+                  chars[left] = chars[right];
+                  chars[right] = temp;
+                  left++;
+                  right--;
+              }
+          }
+          return new String(chars);
+      }
+  }
+  
+  
+
