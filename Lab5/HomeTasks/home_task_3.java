06902a1957c5f1c91bf970c094427577c6da8ec7
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-19 21:04:02 +0500

    task3 of homeTasks

---
+++ Lab5/HomeTasks/home_task_3.java
@@ -0,0 +1,46 @@
+package Lab5.HomeTasks;
+import java.util.Arrays;
+public class home_task_3 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 7, 4, 3, 2, 10, 9, 6, 1};
+        System.out.println("Original Array: " + Arrays.toString(numbers));
+        System.out.println("Sorted Array: " + Arrays.toString(mergeSort(numbers)));
+    }
+    static int[] mergeSort(int[] array) {
+        if (array.length == 1) {
+            return array;
+        }
+        int midIndex = array.length / 2;
+
+        int[] leftHalf = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
+        int[] rightHalf = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+        
+        return merge(leftHalf, rightHalf);
+    }
+    static int[] merge(int[] leftHalf, int[] rightHalf) {
+        int[] sortedArray = new int[leftHalf.length + rightHalf.length];
+        int leftIndex = 0, rightIndex = 0, sortedIndex = 0;
+
+        while (leftIndex < leftHalf.length && rightIndex < rightHalf.length) {
+            if (leftHalf[leftIndex] < rightHalf[rightIndex]) {
+                sortedArray[sortedIndex] = leftHalf[leftIndex];
+                leftIndex++;
+            } else {
+                sortedArray[sortedIndex] = rightHalf[rightIndex];
+                rightIndex++;
+            }
+            sortedIndex++;
+        }
+        while (leftIndex < leftHalf.length) {
+            sortedArray[sortedIndex] = leftHalf[leftIndex];
+            leftIndex++;
+            sortedIndex++;
+        }
+        while (rightIndex < rightHalf.length) {
+            sortedArray[sortedIndex] = rightHalf[rightIndex];
+            rightIndex++;
+            sortedIndex++;
+        }
+        return sortedArray;
+    }
+}
