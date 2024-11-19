4a1380613a26380d724900c7933d455b56cf3045
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-19 17:32:27 +0500

    Task1 of lab5

---
+++ Lab5/Task1.java
@@ -0,0 +1,24 @@
+package Lab5;
+public class Task1 {
+  public static void main(String[] args) {
+    int[] arr = {64, 25, 12, 22, 11};
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;}
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+            System.out.print("Step " + (i + 1) + ": ");
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }}}
+
